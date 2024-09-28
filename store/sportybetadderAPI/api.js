ef3342fa04449c3dce994fe7a9cce74c88b3af5e
Crowdbotics <engineering@crowdbotics.com>
2024-09-28 05:48:06 +0000

    Update store.

--- store/sportybetadderAPI/api.js
+++ store/sportybetadderAPI/api.js
@@ -14,6 +14,58 @@ function api_v1_login_create(payload) {
 function api_v1_signup_create(payload) {
   return sportybetadderAPI.post(`/api/v1/signup/`, payload)
 }
+function modules_disqus_comment_list(payload) {
+  return sportybetadderAPI.get(`/modules/disqus/comment/`, {
+    params: { page: payload.page, page_size: payload.page_size }
+  })
+}
+function modules_disqus_comment_create(payload) {
+  return sportybetadderAPI.post(`/modules/disqus/comment/`, payload)
+}
+function modules_disqus_comment_like_list(payload) {
+  return sportybetadderAPI.get(`/modules/disqus/comment-like/`, {
+    params: { page: payload.page, page_size: payload.page_size }
+  })
+}
+function modules_disqus_comment_like_create(payload) {
+  return sportybetadderAPI.post(`/modules/disqus/comment-like/`, payload)
+}
+function modules_disqus_comment_like_retrieve(payload) {
+  return sportybetadderAPI.get(`/modules/disqus/comment-like/${payload.id}/`)
+}
+function modules_disqus_comment_like_update(payload) {
+  return sportybetadderAPI.put(
+    `/modules/disqus/comment-like/${payload.id}/`,
+    payload
+  )
+}
+function modules_disqus_comment_like_partial_update(payload) {
+  return sportybetadderAPI.patch(
+    `/modules/disqus/comment-like/${payload.id}/`,
+    payload
+  )
+}
+function modules_disqus_comment_like_destroy(payload) {
+  return sportybetadderAPI.delete(`/modules/disqus/comment-like/${payload.id}/`)
+}
+function modules_disqus_comment_retrieve(payload) {
+  return sportybetadderAPI.get(`/modules/disqus/comment/${payload.id}/`)
+}
+function modules_disqus_comment_update(payload) {
+  return sportybetadderAPI.put(
+    `/modules/disqus/comment/${payload.id}/`,
+    payload
+  )
+}
+function modules_disqus_comment_partial_update(payload) {
+  return sportybetadderAPI.patch(
+    `/modules/disqus/comment/${payload.id}/`,
+    payload
+  )
+}
+function modules_disqus_comment_destroy(payload) {
+  return sportybetadderAPI.delete(`/modules/disqus/comment/${payload.id}/`)
+}
 function rest_auth_login_create(payload) {
   return sportybetadderAPI.post(`/rest-auth/login/`, payload)
 }
@@ -57,6 +109,18 @@ export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_disqus_comment_list,
+  modules_disqus_comment_create,
+  modules_disqus_comment_like_list,
+  modules_disqus_comment_like_create,
+  modules_disqus_comment_like_retrieve,
+  modules_disqus_comment_like_update,
+  modules_disqus_comment_like_partial_update,
+  modules_disqus_comment_like_destroy,
+  modules_disqus_comment_retrieve,
+  modules_disqus_comment_update,
+  modules_disqus_comment_partial_update,
+  modules_disqus_comment_destroy,
   rest_auth_login_create,
   rest_auth_logout_create,
   rest_auth_password_change_create,
